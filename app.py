9d9c8c88232b255f85a025faba78abb92182ad39
priyashroff <60972166+priyashroff@users.noreply.github.com>
2021-02-26 23:22:58 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -62,7 +62,7 @@ def sayFunc(phrase):
     # global rate
     # global seconds
 
-    engine = pyttsx3.init()
+    engine = pyttsx3.init('espeak')
     voices = engine.getProperty('voices')
     voices = engine.getProperty('voices')
     for voice in voices:
